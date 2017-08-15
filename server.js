56ec765f6e2a229e3138cbc37f157e6fe55accef
Nithin R <nithinramesh1214@gmail.com>
2017-08-15 10:31:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/article-one',function(req,res){
     res.send("Article one requested and will be served");
 });
 app.get('/article-two',function(req,res){
-    res.send("Article two requested and will be served");
+    res.send("<strong>>Article two requested and will be served</strong>");
 });
 
 app.get('/ui/style.css', function (req, res) {
