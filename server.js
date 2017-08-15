4cb5a726ebde65af905f77cbc78e228b4f4496e5
Nithin R <nithinramesh1214@gmail.com>
2017-08-15 23:35:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/article-one',function(req,res){
     res.send("Article one requested and will be served");
 });
 app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname,'/','article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -32,3 +32,62 @@ var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
+
+
+var articleOne = {
+    title: "Article-one|NithinR",
+    heading: "Article One",
+    date:'aug 15 2017', 
+    content:`
+    <p>
+                This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article.
+            </p>
+            <p>
+                This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article.
+            </p>
+            <p>
+                This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article.
+            </p>
+            <p>
+                This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article. This is the content of my first Article.
+            </p>
+    `
+};
+
+
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date = data.date;
+    var content=data.content;
+    var htmlTemplate =`
+    <html >
+        <head>
+            <title>${title}</title>
+            <meta name="viewport" content="width-device-width,initial-scale=1">
+            <style link="/ui/style.css" rel="stylesheet"></style>    
+        </head>
+        
+        <body>
+            <div class="container">
+            <div>
+                <a href="/">HOME</a>
+            </div>
+            <hr>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+               ${content}
+            </div>
+            </div>
+        </body>
+    
+    </html>
+    `;
+     return htmlTemplate;
+}
